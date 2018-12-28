bb5115faf6f127e30fdb7043e7204d562ad523e6
CR_Avila <carlosrobertoavilah@gmail.com>
2018-12-28 00:42:47 -0600

    Introduce literals and properties

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ const log = (content) => console.log(content);
 let user = {
     name: 'John',
     age: 30,
+    "likes birds": true,
 };
 
 log(`${user.name} is ${user.age} years old`);
 
+user.isAdmin = true;
 delete user.age;
 
 log(user)
+log(user['likes birds']);
+
+delete user['likes birds'];
+
+log(user);
+
 
