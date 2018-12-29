d2c53319fb3196157cd0e97a3cc77bec2e862822
CR_Avila <carlosrobertoavilah@gmail.com>
2018-12-28 18:02:49 -0600

    Finish javascript.info tasks

--- main.js
+++ main.js
@@ -293,6 +293,145 @@ log('Tasks');
 log('')
 log('')
 
+log('1. Hello, Object')
+
+
+/*
+
+Write in the code, one line for each action:
+
+    1. Create an empty object user. 
+
+    2. Add the property name with the value John.
+
+    3. Add the property surname with the value Smith.
+
+    4. Change the value of the name to Pete.
+
+    5. Remove the property name from the object.
+
+*/
+
+log('');
+
+    var user0 = {}
+    log(user0);
+
+    user0['name'] = 'John';
+    log(user0);
+
+    user0['surname'] = 'Smith';
+    log(user0);
+
+    user0['name'] = 'Pete';
+    log(user0);
+
+    delete user0['name'];
+    log(user0);
+
+
+log('');
+log('2 . Check for emptiness')
+log('');
+
+    /*
+
+    Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise
+
+    */
+
+    let emptyObject = {};
+
+    let fullObject = {
+        prop1: 1,
+        prop2: 2,
+        prop3: 3,
+    };
+
+    const isEmpty = (object) => {
+        for(key in object){
+            return false;
+        }
+        return true;
+    }
+
+    log(emptyObject);
+    log(isEmpty(emptyObject));
+    log(fullObject);
+    log(isEmpty(fullObject));
+    
+
+    log('');
+    log('3. Constant objects?');
+    log('');
+
+    //Is it possible to change an object declared with const?
+
+    const constantUser = {
+        name: 'John',
+        age: 30,
+    }
+
+    log(constantUser);
+
+    constantUser['name'] = 'Thomas';
+    log(constantUser);
+
+    //The const only protects the variable itself from changing
+
+
+    log('');
+    log('4. Sum object properties')
+    log('');
+
+    //Write a code to sum all salaries and store them in the variable sum
+
+    let salaries = {
+        John:100,
+        Ann:160,
+        Pete:130,
+    }
+
+    log(salaries);
+
+    let sum = (object) => {
+        let total = 0;
+
+        for(let key in object){
+            total += object[key]
+        }
+        return total;
+    }
+
+    log(sum(salaries));
+
+    log('');
+    log('5. Multiply numeric properties by 2');
+    log('');
+
+    let menu = {
+        width:200,
+        height:300,
+        title: 'My Menu',
+    }
+
+    log(menu);
+
+    const multiplyNumeric = (object) => {
+        for(let num in object){
+            if(typeof object[num] == 'number'){
+                object[num] *= 2;
+            }
+        }
+    }
+
+    multiplyNumeric(menu);
+
+    log(menu);
+
+
+
+
 
 
 
